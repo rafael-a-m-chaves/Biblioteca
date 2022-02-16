bc0e48df469535d6484eff52581823d871e0de25
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-16 14:15:56 -0300

    Adicionar arquivos de projeto.

---
+++ MatematicaOperacao/Soma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatematicaOperacao
+{
+    public class Soma
+    {
+        public static double Some(double x1, double x2)
+        {
+            return x1 + x2;
+        }
+    }
+}
